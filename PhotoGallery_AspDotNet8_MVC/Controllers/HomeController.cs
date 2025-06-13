91454f89ed3e9047cbae60161dc8e8778e9643df
Hsu Pyae Aung <hsupyaeaung@acedatasystems.com>
2025-06-13 20:27:33 +0630

    Add project files.

---
+++ PhotoGallery_AspDotNet8_MVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using PhotoGallery_AspDotNet8_MVC.Models;
+using System.Diagnostics;
+
+namespace PhotoGallery_AspDotNet8_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PhotoGallery_AspDotNet8_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhotoGallery_AspDotNet8_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
